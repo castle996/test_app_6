d4cafe6bc955f21c76d20a62a8301a026e9a0276
Castle996 <bobxia84@gmail.com>
2022-09-21 08:36:16 +0800

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_063910) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_071834) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
